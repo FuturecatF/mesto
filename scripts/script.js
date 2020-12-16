dbd4216171006a481205c6b0fc5259322cbed639
Oleg Futurecat <vtoroysite1@yandex.ru>
2020-12-16 17:53:25 +0300

    сделать рабочий скрипт

--- scripts/script.js
+++ scripts/script.js
@@ -6,7 +6,6 @@ let profileTitle = document.querySelector('.profile__title');
 let profileSubtitle = document.querySelector('.profile__subtitle');
 let popupJob = document.querySelector('.popup__job');
 let popupButton = document.querySelector('.popup__button');
-
 let popupForm = document.querySelector('.popup__form');
 
 function togglePopup() {
@@ -25,15 +24,17 @@ function takePlaceholder() {
   popupJob.placeholder = profileSubtitle.textContent;
 }
 
-  popupForm.onsubmit = function(evt) {
+  function handleFormSubmit(evt) {
   evt.preventDefault();
   profileTitle.textContent = popupName.value;
   profileSubtitle.textContent = popupJob.value;
   togglePopup();
 }
 
+
+
 takePlaceholder();
 editButton.addEventListener('click', togglePopup);
 popupClose.addEventListener('click', togglePopup);
-popupButton.addEventListener('submit', onsubmit);
+popupForm.addEventListener('submit', handleFormSubmit);
 
