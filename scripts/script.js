a0c9432eed9811b7497acd9f71e9b87c844ec8c5
Oleg Futurecat <vtoroysite1@yandex.ru>
2020-12-16 07:00:15 +0300

    feat: сделать скрипт

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,30 @@
-let popUp = document.querySelector('.popup');
+let popup = document.querySelector('.popup');
 let editButton = document.querySelector('.edit-button');
 let popupClose = document.querySelector('.popup__close');
+let popupName = document.querySelector('.popup__name');
+let profileTitle = document.querySelector('.profile__title');
+let profileSubtitle = document.querySelector('.profile__subtitle');
+let popupJob = document.querySelector('.popup__job');
+let popupButton = document.querySelector('.popup__button');
 
 function togglePopup() {
-  popUp.classList.toggle('popup-toggle');
+  popup.classList.toggle('popup-toggle');
 }
 
+function takePlaceholder() {
+  popupName.placeholder = profileTitle.textContent;
+  popupJob.placeholder = profileSubtitle.textContent;
+}
 
+function handleFormSubmit (evt) {
+  evt.preventDefault();
+  let newName = popupName.value;
+  profileTitle.textContent = newName;
+  let newJob = popupJob.value;
+  profileSubtitle.textContent = newJob;
+}
 
-
-
-
+takePlaceholder();
 editButton.addEventListener('click', togglePopup);
 popupClose.addEventListener('click', togglePopup);
+popupButton.addEventListener('submit', handleFormSubmit);
