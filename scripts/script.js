d29188be1667ba92f8b2765040bdc6bfc90ab41b
Oleg Futurecat <vtoroysite1@yandex.ru>
2020-12-16 08:21:18 +0300

    feat: сделать отправку формы

--- scripts/script.js
+++ scripts/script.js
@@ -7,24 +7,33 @@ let profileSubtitle = document.querySelector('.profile__subtitle');
 let popupJob = document.querySelector('.popup__job');
 let popupButton = document.querySelector('.popup__button');
 
+let popupForm = document.querySelector('.popup__form');
+
 function togglePopup() {
   popup.classList.toggle('popup-toggle');
+  clearValue();
 }
+
+function clearValue() {
+  popupName.value = "";
+  popupJob.value = "";
+}
+
 
 function takePlaceholder() {
   popupName.placeholder = profileTitle.textContent;
   popupJob.placeholder = profileSubtitle.textContent;
 }
 
-function handleFormSubmit (evt) {
+  popupForm.onsubmit = function(evt) {
   evt.preventDefault();
-  let newName = popupName.value;
-  profileTitle.textContent = newName;
-  let newJob = popupJob.value;
-  profileSubtitle.textContent = newJob;
+  profileTitle.textContent = popupName.value;
+  profileSubtitle.textContent = popupJob.value;
+  togglePopup();
 }
 
 takePlaceholder();
 editButton.addEventListener('click', togglePopup);
 popupClose.addEventListener('click', togglePopup);
-popupButton.addEventListener('submit', handleFormSubmit);
+popupButton.addEventListener('submit', onsubmit);
+
