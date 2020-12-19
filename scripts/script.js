7bfdf14fe9691aec2ea0c7a35b945cd2f2ece481
Oleg Futurecat <vtoroysite1@yandex.ru>
2020-12-19 06:24:22 +0300

    почти сделать проект

--- scripts/script.js
+++ scripts/script.js
@@ -8,41 +8,36 @@ let profileTitle = document.querySelector('.profile__title');
 let profileSubtitle = document.querySelector('.profile__subtitle');
 let editButton = document.querySelector('.edit-button');
 let likeButtons = document.querySelectorAll('.element__like');
-popupButton.setAttribute('disabled', 'вжух');
 
 function togglePopup() {
   popup.classList.toggle('popup-toggle');
-  takePlaceholder();
-  clearValue();
+  takeInputValue();
   popupButton.classList.remove('popup__button_btn_active');
+  popupButton.setAttribute('disabled', 'вжух');
 }
 
-function clearValue() {
-  popupName.value = "";
-  popupJob.value = "";
-}
-
-function takePlaceholder() {
-  popupName.placeholder = profileTitle.textContent;
-  popupJob.placeholder = profileSubtitle.textContent;
+function takeInputValue() {
+  popupName.value = profileTitle.textContent;
+  popupJob.value = profileSubtitle.textContent;
 }
 
 function handleFormSubmit(evt) {
   evt.preventDefault();
   if (popupName.value.length > 0) {
     profileTitle.textContent = popupName.value;
-  }
+    inputText();
+  } else {return false; }
   if (popupJob.value.length > 0) {
     profileSubtitle.textContent = popupJob.value;
-  }
-  togglePopup();
+    inputText();
+  } else {return false; }
+    togglePopup();
 }
 
 function inputText() {
   popupButton.removeAttribute('disabled', 'вжух');
   popupButton.classList.add('popup__button_btn_active');
 }
-
 
 for (let i = 0; i < likeButtons.length; i++) {
   likeButtons[i].addEventListener('click', function () {
@@ -52,12 +47,7 @@ for (let i = 0; i < likeButtons.length; i++) {
   });
 }
 
-
-
 editButton.addEventListener('click', togglePopup);
 popupClose.addEventListener('click', togglePopup);
 popupForm.addEventListener('submit', handleFormSubmit);
 popupForm.addEventListener('input', inputText);
-
-
-
