950e44209862d8a29104c1413a69d883b2601b0b
Oleg Futurecat <vtoroysite1@yandex.ru>
2021-01-26 16:51:24 +0300

    сделать правки по ревью 2

--- scripts/script.js
+++ scripts/script.js
@@ -20,33 +20,6 @@ const photoLink = popupNewCard.querySelector('.popup__input_type_card-link');
 const cardAdd = document.querySelector('.profile__add-button');
 const elementTemplate = document.querySelector('#element-template').content;
 
-const initialCards = [
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
-];
-
 function toogleModal(modal) {
   modal.classList.toggle('popup_opened');
 }
